789681b08bfa2159e54fbed1acc69c2ba0b0ae36
Jonas Batista <jonas.nb4@gmail.com>
2022-01-17 20:38:40 -0300

    Adcionado o footer com icons de redes sociais

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import Header from "./Components/Header/Header";
-import Main from "./Components/main/Main";
+/** @format */
 
+import Footer from './Components/footer/Footer';
+import Header from './Components/Header/Header';
+import Main from './Components/main/Main';
 
 function App() {
-  return (
-    
-    <>
-    <div className="bg-[#000017] w-full h-screen"> 
-      <Header/>
-      <Main/>
-    </div>
-    </>
-    
-    
-  );
+	return (
+		<>
+			<div className='bg-[#000017] w-full h-screen'>
+				<Header />
+				<Main />
+				<Footer />
+			</div>
+		</>
+	);
 }
 
 export default App;
