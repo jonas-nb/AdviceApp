7a0f60c14f4aaa61da677ff05af2ff77079d0c21
Jonas Batista <jonas.nb4@gmail.com>
2022-01-17 18:44:03 -0300

    Feito as funções do api para receber as frases, também a parte de front do main,

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Header from "./Components/Header/Header";
+import Main from "./Components/main/Main";
 
 
 function App() {
   return (
-    <>
     
+    <>
+    <div className="bg-[#000017] w-full h-screen"> 
+      <Header/>
+      <Main/>
+    </div>
     </>
+    
     
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FaRegCompass } from 'react-icons/fa';
+
+const Header = () => {
+    return (
+        
+            <div className='border-t'>
+                <h1 className='flex items-center text-white justify-around itens-center m-auto mt-6 text-5xl w-52 font-medium shadow-sm   '>Advice<FaRegCompass/></h1>
+            </div>
+        
+    )
+}
+
+export default Header
